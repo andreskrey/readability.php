0a104f93c8187efff03417c6ffed3c6137c71db7
Andres Rey <andres@providentcrm.com>
2016-10-18 15:52:57 +0100

    Fixed a nasty bug when searching for a missing parent on an element. Expanded the Readability object with class and element scoring.

--- src/ReadabilityInterface.php
+++ src/ReadabilityInterface.php
@@ -4,5 +4,12 @@ namespace andreskrey\Readability;
 
 interface ReadabilityInterface
 {
+    public function __construct($node);
+
     public function getScore();
+
+    public function initializeNode();
+
+    public function getClassWeight();
+
 }
