465b0d95fe0b9e46d9953476eb9f3621e5d645a1
Andres Rey <andreskrey@gmail.com>
2017-11-26 19:41:27 +0000

    Add setNodeTag

--- src/NodeUtility.php
+++ src/NodeUtility.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace andreskrey\Readability;
+use andreskrey\Readability\NodeClass\DOMDocument;
+use andreskrey\Readability\NodeClass\DOMNode;
+use andreskrey\Readability\NodeClass\DOMNodeList;
 
 
 /**
@@ -28,4 +31,42 @@ class NodeUtility
 
         return $next;
     }
+
+
+    /**
+     * Changes the node tag name. Since tagName on DOMElement is a read only value, this must be done creating a new
+     * element with the new tag name and importing it to the main DOMDocument.
+     *
+     * @param string $value
+     * @param bool $importAttributes
+     * @return DOMNode
+     */
+    public static function setNodeTag($node, $value, $importAttributes = false)
+    {
+        $new = new DOMDocument();
+        $new->appendChild($new->createElement($value));
+
+        $children = $node->childNodes;
+        /** @var $children DOMNodeList $i */
+
+        for ($i = 0; $i < $children->length; $i++) {
+            $import = $new->importNode($children->item($i), true);
+            $new->firstChild->appendChild($import);
+        }
+
+        if ($importAttributes) {
+            // Import attributes from the original node.
+            foreach ($node->attributes as $attribute) {
+                $new->firstChild->setAttribute($attribute->nodeName, $attribute->nodeValue);
+            }
+        }
+
+        // The import must be done on the firstChild of $new, since $new is a DOMDocument and not a DOMElement.
+        $import = $node->ownerDocument->importNode($new->firstChild, true);
+        $node->parentNode->replaceChild($import, $node);
+
+        return $import;
+    }
+
+
 }
