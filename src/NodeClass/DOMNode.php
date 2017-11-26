81ec9e4bf5990be38c480f36f9f057e0907c613c
Andres Rey <andreskrey@gmail.com>
2017-11-26 20:02:54 +0000

    Add type hinting in DOMNode

--- src/NodeClass/DOMNode.php
+++ src/NodeClass/DOMNode.php
@@ -2,6 +2,12 @@
 
 namespace andreskrey\Readability\NodeClass;
 
+/**
+ * Class DOMNode
+ *
+ * @method getAttribute($attribute)
+ * @package andreskrey\Readability\NodeClass
+ */
 class DOMNode extends \DOMNode
 {
     use NodeClassTrait;
