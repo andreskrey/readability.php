98bbe87936c8ad5a6238dc59569b5237154aa79c
Andres Rey <andreskrey@gmail.com>
2017-11-26 20:02:35 +0000

    Discard node classes that will not be used

--- src/NodeClass/DOMEntityReference.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace andreskrey\Readability\NodeClass;
-
-class DOMEntityReference extends \DOMEntityReference
-{
-    use NodeClassTrait;
-}
--- src/NodeClass/DOMNodeList.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace andreskrey\Readability\NodeClass;
-
-class DOMNodeList extends \DOMNodeList
-{
-    use NodeClassTrait;
-}
