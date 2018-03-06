47c1d1cdd0667f712a9ba56b682dd10639f36bfa
Andres Rey <andreskrey@gmail.com>
2018-03-06 20:16:35 +0000

    Add data-src as a image path source

--- src/Readability.php
+++ src/Readability.php
@@ -1516,6 +1516,7 @@ class Readability
                  */
                 $url = [
                     $img->getAttribute('src'),
+                    $img->getAttribute('data-src'),
                     $img->getAttribute('data-original'),
                     $img->getAttribute('data-orig'),
                     $img->getAttribute('data-url')
