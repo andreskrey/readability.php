45a5ab6f11ce59a2ab9b18eb8c734ad18de86605
Andres Rey <andreskrey@gmail.com>
2019-06-17 23:12:02 +0100

    Minor change

--- src/Nodes/NodeTrait.php
+++ src/Nodes/NodeTrait.php
@@ -504,18 +504,22 @@ trait NodeTrait
             );
     }
 
+    /**
+     * In the original JS project they check if the node has the style display=none, which unfortunately
+     * in our case we have no way of knowing that. So we just check for the attribute hidden or "display: none".
+     *
+     * Might be a good idea to check for classes or other attributes like 'aria-hidden'
+     *
+     * @return bool
+     */
     public function isProbablyVisible()
     {
-        /*
-         * In the original JS project they check if the node has the style display=none, which unfortunately
-         * in our case we have no way of knowing that. So we just check for the attribute hidden or "display: none".
-         *
-         * Might be a good idea to check for classes or other attributes like 'aria-hidden'
-         */
-
         return !preg_match('/display:( )?none/', $this->getAttribute('style')) && !$this->hasAttribute('hidden');
     }
 
+    /**
+     * @return bool
+     */
     public function isWhitespace()
     {
         return ($this->nodeType === XML_TEXT_NODE && mb_strlen(trim($this->textContent)) === 0) ||
